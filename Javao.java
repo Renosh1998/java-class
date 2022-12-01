246d5aac6d3184d4c65eb5a0930cb91a4213a29a
Renosh1998 <118264273+Renosh1998@users.noreply.github.com>
2022-12-01 15:08:40 +0530

    main file

---
+++ Javao.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javao;
+
+/**
+ *
+ * @author welcome
+ */
+public class Javao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        bank_name bname = new bank_name();
+        bname.Bankname ="icici";
+        bname.intrest=7;
+        bname.dass();
+        
+    }
+    
+}
