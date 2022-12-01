d7f42def1206f923f8eb31e69cbddbd8a14c9a21
Renosh1998 <118264273+Renosh1998@users.noreply.github.com>
2022-12-01 15:09:21 +0530

    Add files via upload

---
+++ bank_name.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javao;
+
+/**
+ *
+ * @author welcome
+ */
+public class bank_name {
+    String Bankname;
+    float intrest;
+    void dass (){
+        System.out.println("bankname = " + Bankname + " intrest = "+ intrest);
+    }
+    
+}
