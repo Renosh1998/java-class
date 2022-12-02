a4fcb1f57a31fc9b342732a511fa7a8d3dbdfb49
Renosh1998 <118264273+Renosh1998@users.noreply.github.com>
2022-12-02 15:46:12 +0530

    class _object

---
+++ car/src/car/Car.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package car;
+
+/**
+ *
+ * @author welcome
+ */
+public class Car {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        veh nam = new veh();
+        nam.car_name ="swift";
+        nam.car_model="suzuki";
+        nam.css();
+    }
+    
+}
---
+++ car/src/car/veh.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car;
+
+/**
+ *
+ * @author welcome
+ */
+public class veh {
+    String car_name;
+    String car_model;
+    void css()
+    {
+        System.out.println("carname="+car_name+"carmodel="+car_model);
+    }
+            
+    
+ 
+    
+}
